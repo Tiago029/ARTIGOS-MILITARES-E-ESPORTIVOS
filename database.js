fc4918abd59b21c4aa19eb39af8e328a3e38a294
tiago oliveira de castro <oliveiradecastrotiago963@gmail.com>
2026-08-08 02:09:57 -0300

    fix: conectar banco mysql do railway

--- database.js
+++ database.js
@@ -1,13 +1,15 @@
 const mysql = require("mysql2");
 
 const conexao = mysql.createPool({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    host: process.env.MYSQLHOST || process.env.DB_HOST,
+    port: process.env.MYSQLPORT || process.env.DB_PORT || 3306,
+    user: process.env.MYSQLUSER || process.env.DB_USER,
+    password: process.env.MYSQLPASSWORD || process.env.DB_PASSWORD,
+    database: process.env.MYSQLDATABASE || process.env.DB_NAME,
+
     waitForConnections: true,
-    connectionLimit: 10
+    connectionLimit: 10,
+    queueLimit: 0
 });
 
 module.exports = conexao.promise();
